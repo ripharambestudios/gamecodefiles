5c9fe31d2ff25ae0e5492d6dd4298e1994dce29b
Eric <Eric>
2016-09-30 23:20:19 -0600

    changed animators so shooting anim works correclty

--- Heaven's Essence/Assets/Scripts/AnimatorScript.cs
+++ Heaven's Essence/Assets/Scripts/AnimatorScript.cs
@@ -19,15 +19,17 @@ public class AnimatorScript : MonoBehaviour {
 		var yDiff = Mathf.Abs (this.transform.position.y - lookDirection.y);
 
 
-
 		if (xDiff < yDiff) {
 			//up
 			if (lookDirection.y > this.transform.position.y) {
 				animator.SetInteger ("Direction", 2);
+				fire (2);
+
 			}
 			//down
 			else {
 				animator.SetInteger ("Direction", 0);
+				fire (0);
 			}
 		} 
 
@@ -35,21 +37,26 @@ public class AnimatorScript : MonoBehaviour {
 			//right
 			if (lookDirection.x > this.transform.position.x) {
 				animator.SetInteger ("Direction", 1);
+				fire (1);
 			}
 			//left
 			else {
 				animator.SetInteger ("Direction", 3);
+				fire (3);
 			}
 		}
 			
 
+	}
 
+	void fire(int value)
+	{
 		if (Input.GetAxis ("Fire1") > 0) {
+			animator.SetInteger ("Direction", value);
 			animator.SetBool ("Shoot", true);
 		} 
 		else {
 			animator.SetBool ("Shoot", false);
 		}
-
 	}
 }
--- Heaven's Essence/Assets/Scripts/MeteorAI.cs
+++ Heaven's Essence/Assets/Scripts/MeteorAI.cs
@@ -4,22 +4,19 @@ using System;
 
 public class MeteorAI : MonoBehaviour {
 
-	public float sightRadius = 40f; //max distance can be from player and still in view
+	public float sightRadius = 60f; //max distance can be from player and still in view
 	public float damage = 20f;
-	public float waitTime =0.5f;
-	public float travelSpeed = 10f; //bigger number means slower enemy
-	public float attackCooldown = 1f;
+	public float waitTime =0.25f;
+	public float attackCooldown = .25f;
 
 	private GameObject target;
 	private float distanceToTarget;
 	private bool isAttacking = false;
-	private float launchSpeed;
+	private float launchSpeed = 1f;
 
 	// Use this for initialization
 	void Start () {
 		target = GameObject.FindGameObjectWithTag ("Player");  //may need to tweak this
-		launchSpeed = 1 / travelSpeed;
-
 	}
 	
 	// Update is called once per frame
@@ -48,12 +45,12 @@ public class MeteorAI : MonoBehaviour {
 		Debug.Log (impact.point + "TARGETING PLAYER");
 		yield return new WaitForSeconds (waitTime);
 		bool hasHit = false;
-		float distanceToGo = Math.Abs (distance) * 1.95f;
+		float distanceToGo = Math.Abs (distance) * 2f;
 		if (distanceToGo > 50f) {
 			distanceToGo = 50f;
 		}
 		while(distanceCovered < distanceToGo){
-			nextPosition += look * launchSpeed; //try time.detlatime to see if that can make it better, also look into better name for enemyspeed
+			nextPosition += look.normalized * launchSpeed; //try time.detlatime to see if that can make it better, also look into better name for enemyspeed
 			distanceCovered += Math.Abs (Vector2.Distance (this.transform.position, nextPosition));
 			if (Physics2D.Linecast (this.transform.position, nextPosition, layerMask) && !hasHit) {
 				impact = Physics2D.Linecast (this.transform.position, nextPosition, layerMask);
