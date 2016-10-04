67002d76cb0a51b269d74f5445ef228447404671
packerbacker8 <chandler@gbpackerfans.com>
2016-10-03 19:06:33 -0600

    Pulled Ryan's fixes and added total enemy script

---
+++ Heaven's Essence/Assets/Scripts/TotalEnemies.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TotalEnemies : MonoBehaviour {
+
+	private int totalNumEnemies = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void incrementNumEnemies(int enemies){
+		totalNumEnemies += enemies;
+	}
+
+	public void decrementNumEnemies(){
+		totalNumEnemies--;
+	}
+
+	public bool enemiesGone(){
+		if (totalNumEnemies == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
