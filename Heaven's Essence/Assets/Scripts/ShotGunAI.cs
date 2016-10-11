687221e513da2bb6ac4fea7d5b9ad0446dc25d69
Ryan Steele <ryan.steele.j@gmail.com>
2016-10-11 17:42:31 -0600

    Wrote shotgun AI

--- Heaven's Essence/Assets/Scripts/ShotGunAI.cs
+++ Heaven's Essence/Assets/Scripts/ShotGunAI.cs
@@ -3,39 +3,73 @@ using System.Collections;
 
 public class ShotGunAI : MonoBehaviour {
 
-	public float sightRadius = 8f;
+	public float sightRadius = 3f;
 	public float damage = 20f;
 	public float waitTime = 0.5f;
-	public float inverseLaunchSpeed = 10f;
+	public float inverseLaunchSpeed = 2000f;
 	Vector2 changes = new Vector2 (5, 5);
 	private GameObject target;
 	private float distanceToTarget;
 	private bool isAttacking = false;
 
-	private float attackDistance = 8f;
-
+	private float attackDistance = 10f;
+    private bool stopped = false;
 	public GameObject createProjectile;
 	public GameObject attackType;
 
 	public float attackTime = 2f;
 
+    public int rotateSpeed = 3;
+
 	// Use this for initialization
 	void Start () {
 		isAttacking = false;
 		target = GameObject.FindWithTag ("Player");
-	}
+        transform.LookAt(target.transform.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+    }
 
-	// Update is called once per frame
-	void Update () {
-		distanceToTarget = Vector2.Distance(this.transform.position, target.transform.position);
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector2.Distance(this.transform.position, target.transform.position);
 
-		if (distanceToTarget <= sightRadius && !isAttacking) 
-		{
-			Debug.Log ("Seen");
-			isAttacking = true;
-			StartCoroutine(LaunchAttack());
-		}
-	}
+        if (distanceToTarget <= sightRadius && !isAttacking)
+        {
+            Debug.Log("Seen");
+            isAttacking = true;
+            StartCoroutine(LaunchAttack());
+        }
+
+
+        //Vector2 velocity = new Vector2((transform.position.x - target.transform.position.x - 5) * inverseLaunchSpeed, (transform.position.y - target.transform.position.y - 5) * inverseLaunchSpeed);
+        //GetComponent<Rigidbody2D>().velocity = -velocity;
+
+        if (!stopped)
+        {
+            transform.LookAt(target.transform.position);
+            transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+            if (distanceToTarget > 10f)
+            {//move if distance from target is greater than 1
+
+                //transform.Translate(new Vector3(inverseLaunchSpeed * Time.deltaTime, 0, 0));
+                GetComponent<Rigidbody2D>().AddRelativeForce(transform.right * 100f);
+
+            }
+            else
+            {
+
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                GetComponent<Rigidbody2D>().angularVelocity = 0;
+            }
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().angularVelocity = 0;
+        }
+    }
 
 	//start method for enemy to launch at player
 	IEnumerator LaunchAttack()
@@ -54,18 +88,21 @@ public class ShotGunAI : MonoBehaviour {
 		int layerMask = layerDepth << 8; //player on 8th layer
 		RaycastHit2D impact = Physics2D.Raycast(nextPosition, endLocation, maxDistance, layerMask);
 		Debug.Log (impact.point + "TARGETING PLAYER SHOTGUN");
-		bool hasHit = false;
 		//this.transform.LookAt(target);
-		yield return new WaitForSeconds (waitTime);
+		//yield return new WaitForSeconds (waitTime);
 		//yield return new WaitForSeconds (waitTime);
 		while (distanceToTarget <= sightRadius)
 		{
 			timer += Time.deltaTime;
-			Vector2 velocity = new Vector2 ((transform.position.x - target.transform.position.x - 5) * inverseLaunchSpeed, (transform.position.y - target.transform.position.y - 5) * inverseLaunchSpeed);
-			GetComponent<Rigidbody2D> ().velocity = -velocity;
-			if (timer >= attackTime && distanceToTarget <= attackDistance)
+            			
+			if (timer >= attackTime)
 			{
-				Instantiate(attackType, transform.position, Quaternion.identity);
+                stopped = true;
+                //yield return new WaitForSeconds(0.15f);
+				GameObject bullet = (GameObject)Instantiate(attackType, transform.position + 1.0f*transform.right, transform.rotation);
+                bullet.GetComponent<Rigidbody2D>().AddForce(bullet.transform.right * 5000f);
+                yield return new WaitForSeconds(1);
+                stopped = false;
 				timer = 0f;
 			}
 			yield return null;
--- Heaven's Essence/Assets/Scripts/ShotGunBullet.cs
+++ Heaven's Essence/Assets/Scripts/ShotGunBullet.cs
@@ -6,18 +6,25 @@ public class ShotGunBullet : MonoBehaviour {
 	public float expandSize = 1.33f;
 	public int speed = 10;
 	private float timer = 0f;
-	// Use this for initialization
-	void Start () {
+    private float destroyTimer = 0f;
+    // Use this for initialization
+    void Start () {
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
+        destroyTimer += Time.deltaTime;
 		if (timer >= 1f) {
-			this.transform.localScale = (this.transform.localScale)*2;
-			GetComponent<Rigidbody2D> ().AddForce (transform.up * speed * Time.smoothDeltaTime);
+			//this.transform.localScale;
+            
 			timer = 0f;
 		}
+
+        if(destroyTimer > 5f)
+        {
+            Destroy(this.gameObject);
+        }
 	}
 }
