19f3fb4a48d893d336490034bf05b8a10d71d9af
gpsgamer <sutyak.geoffrey@gmail.com>
2016-12-03 15:19:22 -0700

    modified neemyHealth script so they are invincible for a second when ready to be absorbed. Also added a light the pulses when enemies are ready to be absorbed.

--- Heaven's Essence/Assets/Scripts/EnemyHealth.cs
+++ Heaven's Essence/Assets/Scripts/EnemyHealth.cs
@@ -1,99 +1,121 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyHealth : MonoBehaviour {
+public class EnemyHealth : MonoBehaviour
+{
 
-	public int startHealth = 100;
-	public int scoreValue= 0;
-	private GameObject enemyManager;
-	public AudioClip enemyHitSound;
+    public int startHealth = 100;
+    public int scoreValue = 0;
+    private GameObject enemyManager;
+    public AudioClip enemyHitSound;
 
-	private AudioSource source;
+    private AudioSource source;
 
-	private int currentHealth;
-	private GameObject player;
+    private int currentHealth;
+    private GameObject player;
 
-	public GameObject pool;
-	private float timeForFlashGold = .5f;
-	private float timeForFlashRed = .1f;
-	public string poolName;
+    public GameObject pool;
+    private bool pulseGold;
+    private float timeForFlashRed = .1f;
+    private float timeForInvinciblity = 0;
+    private bool invincible = false;
+    public string poolName;
 
-	// Use this for initialization
-	void Start ()
-	{
-		currentHealth = startHealth;
-		enemyManager = GameObject.FindGameObjectWithTag ("Enemy Manager");
-		player = GameObject.FindGameObjectWithTag ("Player");
-		source = gameObject.AddComponent<AudioSource> ();
-		pool = GameObject.FindGameObjectWithTag(poolName);
 
-	}
 
-	// Message sent from player that does damage to enemy
-	public void DealDamage (int damage)
-	{
-		source.PlayOneShot (enemyHitSound, .05f);
-		currentHealth -= damage;
-		if (damage == 1000000) {
-			scoreValue -= (scoreValue / 5);
-		}
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = startHealth;
+        enemyManager = GameObject.FindGameObjectWithTag("Enemy Manager");
+        player = GameObject.FindGameObjectWithTag("Player");
+        source = gameObject.AddComponent<AudioSource>();
+        pool = GameObject.FindGameObjectWithTag(poolName);
 
-		if (currentHealth <= 0) {
-			enemyManager.GetComponent<EnemySpawner> ().decrementNumOfEnemies ();
-			player.SendMessage ("UpdateScore", scoreValue, SendMessageOptions.DontRequireReceiver);
-			int numOfObjects = this.transform.childCount;
-			for(int i =0; i < numOfObjects; i++)
-			{
-			    Destroy(this.transform.GetChild(i));
-			}
-			Destroy (this.gameObject);
 
-			currentHealth = startHealth;
-			pool.GetComponent<PoolingSystem>().returnToPool(gameObject);
-		}
+    }
 
-		if (currentHealth > 0)
-		{
-			StartCoroutine (flashRed ());
-		}
-	}
+    // Message sent from player that does damage to enemy
+    public void DealDamage(int damage)
+    {
+        if (IsBelowTwentyPercent() && !invincible)
+            timeForInvinciblity = 1f;
+        invincible = true;
 
-	public int GetEnemyHealth()
-	{
-		return currentHealth;
-	}
+        if (timeForInvinciblity > 0)
+        {
+            timeForInvinciblity -= Time.deltaTime;
+        }
+        else
+        {
+            source.PlayOneShot(enemyHitSound, .05f);
+            currentHealth -= damage;
+            if (damage == 1000000)
+            {
+                scoreValue -= (scoreValue / 5);
+            }
 
-	public bool IsBelowTwentyPercent()
-	{
-		int aTenth = startHealth / 10;
-		if(currentHealth <= aTenth*2)
-		{
+            if (currentHealth <= 0)
+            {
+                enemyManager.GetComponent<EnemySpawner>().decrementNumOfEnemies();
+                player.SendMessage("UpdateScore", scoreValue, SendMessageOptions.DontRequireReceiver);
+                int numOfObjects = this.transform.childCount;
+                //for(int i =0; i < numOfObjects; i++)
+                //{
+                //    Destroy(this.transform.GetChild(i));
+                //}
+                //Destroy (this.gameObject);
+                currentHealth = startHealth;
+                pool.GetComponent<PoolingSystem>().returnToPool(gameObject);
+            }
 
-			timeForFlashGold -= Time.deltaTime;
-			if (timeForFlashGold <= 0)
-			{
-				if (this.gameObject.GetComponent<SpriteRenderer> ().color == new Color (1, 1, 1, 1))
-				{
-					this.gameObject.GetComponent<SpriteRenderer> ().color = new Color (1, .925f, 0, 1);
-				} 
-				else 
-				{
-					this.gameObject.GetComponent<SpriteRenderer> ().color = new Color (1, 1, 1, 1);
-				}
-				timeForFlashGold = .5f;
-			}
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+            if (currentHealth > 0)
+            {
+                StartCoroutine(flashRed());
+            }
+        }
+    }
 
-	IEnumerator flashRed()
-	{
-		this.gameObject.GetComponent<SpriteRenderer> ().color = new Color (1, .17647f, .17647f, 1);
-		yield return new WaitForSeconds (timeForFlashRed);
-		this.gameObject.GetComponent<SpriteRenderer> ().color = new Color (1, 1, 1, 1);
-	}
+    public int GetEnemyHealth()
+    {
+        return currentHealth;
+    }
+
+    public bool IsBelowTwentyPercent()
+    {
+        int aTenth = startHealth / 10;
+        if (currentHealth <= aTenth * 3.5f)
+        {
+            if (timeForInvinciblity <= 0)
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color(.5f, .5f, .5f, 1);
+
+            if (pulseGold == true)
+            {
+
+                this.gameObject.GetComponentInChildren<Light>().intensity += .1f;
+
+                if (this.gameObject.GetComponentInChildren<Light>().intensity >= 1.5f)
+                    pulseGold = false;
+            }
+            else
+            {
+                this.gameObject.GetComponentInChildren<Light>().intensity -= .1f;
+
+                if (this.gameObject.GetComponentInChildren<Light>().intensity <= 0)
+                    pulseGold = true;
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    IEnumerator flashRed()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color = new Color(1, .17647f, .17647f, 1);
+        yield return new WaitForSeconds(timeForFlashRed);
+        this.gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+    }
 }
